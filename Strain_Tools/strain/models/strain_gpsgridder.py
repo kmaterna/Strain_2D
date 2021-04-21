bug fix for gpsgridder and np.gradient

Strain_Tools/strain/models/strain_gpsgridder.py
@@ -73,12 +73,12 @@ def compute_gpsgridder(myVelfield, range_strain, inc, poisson, fd, eigenvalue, t
     xinc = xinc * 111.000 * np.cos(np.deg2rad(range_strain[2]));  # in km (not degrees)
     yinc = yinc * 111.000;   # in km (not degrees)
 
-    # the strain calculation
     [exx, eyy, exy, rot] = strain_tensor_toolbox.strain_on_regular_grid(xinc, yinc, udata, vdata)
+    # Lastly we multiply by 1000 for units
     exx = np.multiply(exx, 1000);
     exy = np.multiply(exy, 1000);
     eyy = np.multiply(eyy, 1000);
-    rot = np.multiply(rot, 1000);
-    print("Success computing strain via gpsgridder method.\n");
+    rot = abs(np.multiply(rot, 1000));
 
+    print("Success computing strain via gpsgridder method.\n");
     return [xdata, ydata, rot, exx, exy, eyy];

Strain_Tools/strain/strain_tensor_toolbox.py
@@ -8,8 +8,9 @@ import math as m
 
 def strain_on_regular_grid(dx, dy, V1, V2):
     '''Compute strain rate on a regular grid'''
-    e11, dV1dx2 = np.gradient(V1, dx, dy)
-    dV2dx1, e22 = np.gradient(V2, dx, dy)
+    # Seems to return y-direction derivatives followed by x-direction derivatives
+    dV1dx2, e11 = np.gradient(V1, dy, dx)
+    e22, dV2dx1 = np.gradient(V2, dy, dx)
 
     # shear strain rate is the symmetric part of the gradient
     e12 = 0.5*(dV1dx2 + dV2dx1)