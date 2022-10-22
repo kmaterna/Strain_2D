14cecfc6eb48e2b0a281fc6fc0552fd6a247b9c7
kmaterna <kathrynmaterna@gmail.com>
2022-10-21 21:52:40 -0700

    reporting misfit for wavelets compute function

--- Strain_Tools/strain/models/strain_wavelets.py
+++ Strain_Tools/strain/models/strain_wavelets.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.interpolate as interp
 import subprocess
 from strain.models.strain_2d import Strain_2d
-from .. import utilities
+from .. import utilities, velocity_io
 
 
 class wavelets(Strain_2d):
@@ -37,9 +37,14 @@ class wavelets(Strain_2d):
         # Not sure whether Wavelets gives velocities or not
         Ve, Vn = np.nan*np.empty(exx.shape), np.nan*np.empty(exx.shape),
 
+        # Get residuals
+        resid_file = output_tag + "_vfield_residual.dat";
+        residual_velfield = report_on_misfits_wavelets(resid_file);
+        print(residual_velfield);
+
         # Report observed and residual velocities within bounding box
         filtered_velfield = utilities.filter_by_bounding_box(myVelfield, self._strain_range);
-        residual_velfield = [];
+        residual_velfield = utilities.filter_by_bounding_box(residual_velfield, self._strain_range);
         return [Ve, Vn, rot, exx, exy, eyy, filtered_velfield, residual_velfield];
 
 
@@ -123,8 +128,7 @@ def input_wavelets(coordsfile, datafile, wfile):
     and outputs the coordinates and strain tensor components.    
     """
     incoords = np.loadtxt(coordsfile, usecols=(0, 1), unpack=True)
-    lon = incoords[0]
-    lat = incoords[1]
+    lon, lat = incoords[0], incoords[1]
     infile = np.loadtxt(datafile, skiprows=1, usecols=(3, 4, 5), unpack=True)
     thth = infile[0]
     thph = infile[1]
@@ -172,14 +176,16 @@ def nn_interp(x, y, vals, newx, newy):
     return newx, newy, newvals
 
 
-def report_on_misfits_wavelets(residfile, key):
-    print("\n" + key.upper());
-    [Sn, Se, resid_Vn, resid_Ve] = np.loadtxt(residfile, usecols=(3, 4, 6, 7), unpack=True);
+def report_on_misfits_wavelets(residfile):
+    [elon, nlat, Sn, Se, resid_Vn, resid_Ve] = np.loadtxt(residfile, usecols=(0, 1, 3, 4, 6, 7), unpack=True);
     #  From Compearth code on Matlab file:
     #  fprintf(fid, stfmt, dlon(ii), dlat(ii), su(ii) * 1e3, sn(ii) * 1e3, se(ii) * 1e3, Vmat(ii,:));
-    _misfit_total, _chi2_total = compute_misfits(resid_Ve, resid_Vn, Se, Sn);
-    return;
-# 'wavelets': ['output/wavelets/matlab-output-files/_d-01_q04_q07_b1_2D_s1_u1_vfield_residual.dat'],
+    residfield = [];
+    for i in range(len(elon)):
+        new_resid = velocity_io.StationVel(elon=elon[i], nlat=nlat[i], e=resid_Ve[i], n=resid_Vn[i], u=0, se=0, sn=0, su=0, name='');
+        residfield.append(new_resid);
+    return residfield;
+
 
 """
 Steps for Tape Wavelets:
