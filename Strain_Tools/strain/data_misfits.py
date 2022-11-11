250a76230d945762a93ad8baca3971a1422b264e
kmaterna <kathrynmaterna@gmail.com>
2022-11-11 15:32:38 -0800

    print the fraction of data excluded by saying resid < 10 mm

--- Strain_Tools/strain/data_misfits.py
+++ Strain_Tools/strain/data_misfits.py
@@ -14,13 +14,16 @@ def compute_misfits(resid_vels, obs_vels, outlier_tolerance=10.0):
     :returns: list of misfits [mm/yr], list of chi2 [unitless]
     """
     misfit_total, chi2_total = [], [];
+    outlier_count = 0;
     for resid, obs in zip(resid_vels, obs_vels):
         if np.abs(resid.e) > outlier_tolerance or np.abs(resid.n) > outlier_tolerance:
+            outlier_count += 1;
             continue;
         misfit_total.append(abs(resid.e));
         misfit_total.append(abs(resid.n));
         chi2_total.append(np.square(resid.e) / np.square(obs.se));
         chi2_total.append(np.square(resid.n) / np.square(obs.sn));
+    print("Percentage of residuals outside tolerance: %f" % (100*outlier_count/len(resid_vels)) );
     print("Median absolute deviation:", np.median(misfit_total))
     print("Median chi2:", np.round(np.median(chi2_total), 5));
     return misfit_total, chi2_total;
